a7fae1e4becf5a8b1717433667f41cc88dd5e17c
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2022-01-12 16:21:17 +0200

    create Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'react-bootstrap';
 
-const Header = () => {
-    return <div></div>;
+const Header: React.FunctionComponent = () => {
+    return (
+        <Navbar fixed='top' bg='dark' variant='dark'>
+            <Container>
+                <NavbarBrand>TypeScript Practice</NavbarBrand>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default Header;
