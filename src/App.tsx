7af14651e2dc09b47ec35c460883d4947d755591
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2022-01-12 13:57:28 +0200

    add Note interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Note } from './models/note.model';
 
 function App() {
-   
-  return <div className='App'>test</div>;
+    const [notes, setNotes] = useState<Note[]>([
+        {
+            id: new Date().toString(),
+            title: 'Meetings',
+            text: 'Schedule meeting with UI/UX team',
+            color: '#dfdfdf',
+            date: new Date().toString(),
+        },
+    ]);
+
+    return <div className='App'>test</div>;
 }
 
 export default App;
