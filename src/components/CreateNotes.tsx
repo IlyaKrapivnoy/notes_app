aaaf6e754bad13f54e38de5c0a768ea224ef20b7
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2022-01-12 17:40:47 +0200

    add useRef and types for it

--- src/components/CreateNotes.tsx
+++ src/components/CreateNotes.tsx
@@ -4,6 +4,10 @@ import { Button, Form } from 'react-bootstrap';
 interface ICreateNotesProps {}
 
 const CreateNotes: React.FC<ICreateNotesProps> = (props) => {
+    const titleRef = useRef<HTMLInputElement | null>(null);
+    const textRef = useRef<HTMLTextAreaElement | null>(null);
+    const colorRef = useRef<HTMLInputElement | null>(null);
+
     return (
         <>
             <h2>Create Notes</h2>
@@ -13,7 +17,8 @@ const CreateNotes: React.FC<ICreateNotesProps> = (props) => {
                     <Form.Control
                         type='text'
                         placeholder='Enter Title for the Note'
-                    ></Form.Control>
+                        ref={titleRef}
+                    />
                 </Form.Group>
                 <Form.Group className='mb-3' controlId='formBasic'>
                     <Form.Label>Text</Form.Label>
@@ -21,7 +26,8 @@ const CreateNotes: React.FC<ICreateNotesProps> = (props) => {
                         placeholder='Enter Your Notes'
                         as='textarea'
                         rows={3}
-                    ></Form.Control>
+                        ref={textRef}
+                    />
                 </Form.Group>
                 <Form.Group className='mb-3'>
                     <Form.Label htmlFor='colorInput'>Notes Color</Form.Label>
@@ -30,6 +36,7 @@ const CreateNotes: React.FC<ICreateNotesProps> = (props) => {
                         id='colorInput'
                         defaultValue='#dfdfdf'
                         title='Choose your color'
+                        ref={colorRef}
                     />
                     <Button type='submit' variant='primary'>
                         Submit
